0f26c877fc72f8c12d0a34d689b2b38ca1016d8a
Lhachhen12 <shreejanlama608@gmail.com>
2025-07-17 21:51:26 +0545

    feat: changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const API_URL = process.env.VITE_APP_API_URL || 'http://localhost:5000';
+// const API_URL = process.env.VITE_APP_API_URL || 'http://localhost:5000';
 // // Then use `${API_URL}/api/auth/register`
-export {API_URL};
-// const API_URL = import.meta.env.VITE_APP_API_URL ;
-// export { API_URL };
+// export {API_URL};
+const API_URL = import.meta.env.VITE_APP_API_URL ;
+export { API_URL };
